df0fd108511e4c6a8d6955459bb41626d1c33017
AlAmin008 <piashpiash222@gmail.com>
2023-12-11 15:59:59 +0600

    profile Info fecthed

--- src/app/pages/profile-builder/Profile.tsx
+++ src/app/pages/profile-builder/Profile.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 // import {KTIcon} from '../../../../../../_metronic/helpers'
 import * as Yup from 'yup'
 import {useFormik} from 'formik'
@@ -8,6 +8,7 @@ import {
   // updateEmail,
   updatePassword,
 } from '../../modules/accounts/components/settings/SettingsModel'
+import { resolveSoa } from 'dns'
 // import {KTIcon} from '../../../_metronic/helpers'
 
 // const emailFormValidationSchema = Yup.object().shape({
@@ -41,8 +42,63 @@ const Profile: React.FC = () => {
   // const [emailUpdateData, setEmailUpdateData] = useState<IUpdateEmail>(updateEmail)
   const [passwordUpdateData, setPasswordUpdateData] = useState<IUpdatePassword>(updatePassword)
   const [showPasswordForm, setPasswordForm] = useState<boolean>(false)
-
+  const [userData, setUserData] = useState()
+  var authtoken ="yJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzAyMjk1MDM4LCJpYXQiOjE3MDIyODYzOTksImp0aSI6IjdmMmFiY2Y2Y2EzMzRlZTk4NzgwNmViODczNjhjYmE1IiwidXNlcl9pZCI6MjZ9.uq_WwE1YOKGVmVJgFDfFIbP7lzfKuPuVVfVedBDkEM8"
   // const [loading1, setLoading1] = useState(false)
+  const fetchData = async () =>{
+    try {
+      const response = await fetch('http://localhost:8000/api/user/profile/',{
+        headers: {
+          Authorization: `Bearer ${authtoken}`, // Replace with your actual authorization token
+          // Add other headers if needed
+        },
+      });
+      
+      if (response.ok){
+        const data = await response.json();
+        setUserData(data);
+        console.log(data)
+      }
+
+      if (response.status === 401){
+        const new_request = await fetch('http://localhost:8000/api/token/refresh/',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({"refresh":"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcwMjM3Mjc5OSwiaWF0IjoxNzAyMjg2Mzk5LCJqdGkiOiIxZjk4OGM0MDE2MjU0MTgxODQyOWU4ZjE0MTg3ZTNkZiIsInVzZXJfaWQiOjI2fQ.LWFmXMuTwj95E6eO-xg3Q8uTckk2g5QV-xAkYkm-GSU"}),
+        })
+        if(new_request.ok){
+          const new_access_token = await new_request.json()
+          console.log(new_access_token['access'])
+          
+          const response_2 = await fetch('http://localhost:8000/api/user/profile/',{
+          headers: {
+          Authorization: `Bearer ${new_access_token['access']}`,
+          },
+          });
+          if (response_2.ok){
+            const data = await response_2.json();
+            setUserData(data);
+            console.log(data)
+          }
+        }
+
+      }
+      
+    }
+    
+    
+    catch (error) {
+      console.error('Error fetching user data:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch user data when the component mounts
+    fetchData();
+  }, []);
+
   const [loading2, setLoading2] = useState(false)
   const formik2 = useFormik<IUpdatePassword>({
     initialValues: {
@@ -65,14 +121,15 @@ const Profile: React.FC = () => {
           <div className='d-flex flex-wrap align-items-center'>
             <div >
               <div className='fs-6 fw-bolder mb-1'>Name</div>
-              <div className='fw-bold text-gray-600'>Armanur Rashid</div>
+              {userData && (<div className='fw-bold text-gray-600'>{userData['name']}</div>)}
+              
             </div>
           </div>
           <div className='separator separator-dashed my-6'></div>
           <div className='d-flex flex-wrap align-items-center'>
             <div id='kt_signin_email'>
               <div className='fs-6 fw-bolder mb-1'>Email Address</div>
-              <div className='fw-bold text-gray-600'>armanurrashid105086@gmail.com</div>
+              {userData && (<div className='fw-bold text-gray-600'>{userData['email']}</div>)}
             </div>
           </div>
 
