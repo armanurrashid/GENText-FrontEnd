3931626d46aaa3ae2e558fbc42deb8ad8c6152d3
Armanur Rashid <58313733+armanurrashid@users.noreply.github.com>
2024-01-17 14:48:42 +0600

    Update _requests.ts

--- src/app/modules/auth/core/_requests.ts
+++ src/app/modules/auth/core/_requests.ts
@@ -51,5 +51,5 @@ export function getUserByToken(token: string) {
 }
 
 // export const URL = 'http://localhost:8000'
-export const URL = 'http://16.0.0.121:8000/'
+export const URL = 'https://207.180.252.5:8000/'
 
